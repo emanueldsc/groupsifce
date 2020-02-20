34c836969c4016207d8ac840e9e4b9435e9c4d8c
Emanuel Douglas Sousa Costa <emanuel.douglas.sc@gmail.com>
2020-02-19 23:52:46 -0300

    Updates

---
+++ precache-manifest.9eea4daa518e5bb8ff228f2b575e59e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f45707dc300dd01331a7d26d3b08bec",
+    "url": "/groupsifce/index.html"
+  },
+  {
+    "revision": "40062b00ab7b164b1cc9",
+    "url": "/groupsifce/static/css/main.c2881ef7.chunk.css"
+  },
+  {
+    "revision": "9844df65bbcd71416505",
+    "url": "/groupsifce/static/js/2.6e8bf58b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/groupsifce/static/js/2.6e8bf58b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40062b00ab7b164b1cc9",
+    "url": "/groupsifce/static/js/main.bcfb063b.chunk.js"
+  },
+  {
+    "revision": "5b2a5f1cbd90eba0c424",
+    "url": "/groupsifce/static/js/runtime-main.5e0ada5d.js"
+  },
+  {
+    "revision": "0bf280388937448d38392b76c15bd441",
+    "url": "/groupsifce/static/media/background.0bf28038.jpg"
+  }
+]);
